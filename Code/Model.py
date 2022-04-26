61f2dcfc26dcb37c860079d86850601433c394b4
gitFloyd <dfincher@kent.edu>
2022-04-25 21:43:57 -0400

    added Model.py

--- Code/Model.py
+++ Code/Model.py
@@ -1,25 +1,88 @@
-
+from Activation import Activation as Act
 
 class Layer:
-	def __init__(self, size:int) -> None:
+
+	SOFTMAX = 1
+	RELU = 2
+
+	def __init__(self, size:int, activation:int = None) -> None:
 		self.size_ = size
+		if (activation == None or activation not in [Layer.SOFTMAX, Layer.RELU]):
+			activation = Layer.RELU
+		self.activation_ = activation
 	
-	def Execute(weights:list[float]):
-		pass
+	def Execute(self, X:list[float], weights:list[float]) -> list[float]:
+		return []
 
 	def Size(self) -> int:
 		return self.size_
 
+	def NumWeights(self, numInputs:int) -> int:
+		return numInputs
+	
+class DeepLayer(Layer):
+	def Execute(self, X:list[float], weights:list[float]) -> list[float]:
+		if self.activation_ == Layer.RELU: 
+			return [
+				Act.ReLU(sum([
+					x * weights[i + j * (len(X) - 1)] for (j, x) in enumerate(X)
+				])) for i in range(self.size_)
+			]
+		else:
+			outputs = [
+				sum([
+					x * weights[i + j * (len(X) - 1)] for (j, x) in enumerate(X)
+				]) for i in range(self.size_)
+			]
+			return [Act.Softmax(outputs, output) for output in outputs]
+
+	def NumWeights(self, numInputs:int) -> int:
+		return self.size_ * numInputs
+	
+class SparseLayer(Layer):
+	def __init__(self, connections:list[tuple[int, list[int]]] = None, activation:int = None) -> None:
+		nodes = set()
+		self.connections_ = connections
+		for connection in connections:
+			nodes.union(connection[1])
+		super.__init__(len(nodes), activation)
+	
+	def Execute(self, X:list[float], weights:list[float]) -> list[float]:
+
+		values = [0] * self.size_
+		for connection in self.connections_:
+			for index in connection[1]:
+				values[index] += X[connection[0]] * weights[index]
+
+		if self.activation_ == Layer.RELU: 
+			return [Act.ReLU(value) for value in values]
+		else:
+			return [Act.Softmax(values, value) for value in values]
+
+	def NumWeights(self, numInputs:int) -> int:
+		return self.size_ * numInputs
 
 class Model:
 
-	def __init__(self) -> None:
-		self.layers_ = []
+	def __init__(self, layers:list[Layer] = []) -> None:
+		self.layers_ = layers
 
 	def AddLayer(self, layer:Layer) -> None:
 		self.layers_.append(layer)
 
 	def Execute(self, X:list[float], weights:list[float]) -> list[float]:
-		[layer for layer in self.layers_]
-		if True:
-			pass
+		start = 0
+		end = 0
+		results = X
+		for layer in self.layers_:
+			start = end
+			end += layer.NumWeights(len(results))
+			results = layer.Execute(results, weights[start:end])
+		return results
+
+	def NumWeights(self, size:int) -> int:
+		count = 0
+		for layer in self.layers_:
+			count += layer.NumWeights(size)
+			size = layer.Size()
+		return count
